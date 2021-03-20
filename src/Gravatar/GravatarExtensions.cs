ae646403fa734699097a78141694488917da39c6
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2021-03-20 10:33:41 +0000

    Add solution and projects

---
+++ src/Gravatar/GravatarExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Gravatar
+{
+    /// <summary>
+    /// Extension methods for converting an email to a Gravatar URL.
+    /// </summary>
+    public static class GravatarExtensions
+    {
+        /// <summary>
+        /// Convert an email to a Gravatar URL.
+        /// </summary>
+        /// <remarks>
+        /// There is no validation to ensure a valid email.
+        /// <see langword="null" />, empty <see cref="string"/> and blank spaces result in an empty URL (<see cref="string.Empty"/>).
+        /// </remarks>
+        /// <param name="email">The email.</param>
+        /// <returns>The Gravatar URL.</returns>
+        public static string ToGravatar(this string email)
+        {
+            const string GravatarAvatarEndpoint = "https://www.gravatar.com/avatar/";
+            const string HexadecimalStringFormat = "x2";
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return string.Empty;
+            }
+
+            using (var md5 = MD5.Create())
+            {
+                var emailAsciiBytes = Encoding.ASCII.GetBytes(email);
+                var hashedBytes = md5.ComputeHash(emailAsciiBytes);
+
+                var hashedHexStringBuilder = new StringBuilder();
+                foreach (var hashedByte in hashedBytes)
+                {
+                    hashedHexStringBuilder.Append(hashedByte.ToString(HexadecimalStringFormat));
+                }
+
+                return $"{GravatarAvatarEndpoint}{hashedHexStringBuilder}";
+            }
+        }
+    }
+}
---
+++ tests/Gravatar.UnitTests/GravatarExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Gravatar.UnitTests
+{
+    public class GravatarExtensionsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("   ")]
+        public void ToGravatar_WhenCallingStaticClassMethod_WithEmptyOrNullOrWhiteSpaceEmail_ShouldReturnEmptyUrl(string email)
+        {
+            // Arrange
+
+            // Act
+            string gravatarUrl = email.ToGravatar();
+
+            // Assert
+            Assert.Empty(gravatarUrl);
+        }
+
+        [Theory]
+        [InlineData("admin@example.org", "96614ec98aa0c0d2ee75796dced6df54")]
+        [InlineData("test@example.com", "55502f40dc8b7c769880b10874abc9d0")]
+        public void ToGravatar_WhenCallingAsStringExtensionMethod_WithEmail_ShouldCreateGravatarUrl(string email, string expectedHash)
+        {
+            // Arrange
+            const string GravatarAvatarEndpoint = "https://www.gravatar.com/avatar/";
+
+            // Act
+            string gravatarUrl = email.ToGravatar();
+
+            // Assert
+            Assert.Equal($"{GravatarAvatarEndpoint}{expectedHash}", gravatarUrl);
+        }
+    }
+}
